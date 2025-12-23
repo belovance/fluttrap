6dd134de0b6dea78000eb439d72610dca0292509
Akash S <as99281@gmail.com>
2025-12-24 04:01:01 +0530

    Improves package metadata and shortens description
    Streamlines the package description for clarity and conciseness.
    Adds metadata fields for documentation, issue tracking, topics, funding,
    and screenshots to enhance discoverability and project transparency.
    
    Added empty example project

---
+++ example/README.md
@@ -0,0 +1,3 @@
+# Fluttrap Example
+
+An example Flutter application for the [fluttrap](https://github.com/belovance/fluttrap) package.
---
+++ example/android/app/src/main/kotlin/com/belovance/fluttrap/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.belovance.fluttrap
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
